da73cb4774c24cf3cd6f023d8d5d27b69c8d68ef
kipchirchiralb <albertkipchirchir02@gmail.com>
2021-10-07 19:25:08 +0300

    stuck at checking buttons

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+// DOM VARIABLES
 const addTaskInput = document.querySelector("#add-task");
 const myList = document.querySelector("#to-do-list");
 const addTaskBtn = document.querySelector("#add-task-btn");
 const clearInputField = document.querySelector("#clear-input-field");
 const showCompleteBtn = document.querySelector("#show-complete-btn");
 const hideCompleteBtn = document.querySelector("#hide-complete-btn");
-const clearList = document.querySelector("clear-all-tasks");
+const checkBtns = document.querySelectorAll(".unchecked-btn");
+const completeTasks = document.querySelectorAll(".checked-btn");
+const clearList = document.querySelector("#clear-all-tasks");
+
+// EVENT LISTENERS
 
 // adding a task to the task list and clearing the input field
 addTaskInput.addEventListener("keypress", function (e) {
@@ -19,9 +24,16 @@ addTaskBtn.addEventListener("click", addTaskToList);
 clearInputField.addEventListener("click", function (e) {
   addTaskInput.value = "";
 });
+
 clearList.addEventListener("click", function (e) {
   myList.innerHTML = "";
 });
+
+// toggle between showing and hiding complete tasks
+showCompleteBtn.addEventListener("click", showCompleteTasks);
+hideCompleteBtn.addEventListener("click", hideCompleteTasks);
+
+// FUNCTIONS
 
 function addTaskToList() {
   if (addTaskInput.value.length == 0) {
@@ -32,6 +44,7 @@ function addTaskToList() {
     let newItem = document.createElement("li");
     let indicatorBtn = document.createElement("button");
     indicatorBtn.textContent = "";
+    indicatorBtn.classList.add("unchecked-btn");
     let textNode = document.createTextNode(text);
     newItem.append(textNode);
     newItem.appendChild(indicatorBtn);
@@ -40,36 +53,24 @@ function addTaskToList() {
   }
 }
 
-// toggle between showing and hiding complete tasks
-showCompleteBtn.addEventListener("click", showCompleteTasks);
-hideCompleteBtn.addEventListener("click", hideCompleteTasks);
-
 function showCompleteTasks() {
   hideCompleteBtn.classList.remove("hide");
   showCompleteBtn.classList.add("hide");
+  completeTasks.classList.remove("hide");
 }
 
 function hideCompleteTasks() {
   showCompleteBtn.classList.remove("hide");
   hideCompleteBtn.classList.add("hide");
+  completeTasks.classList.add("hide");
 }
 
-// function bindButtons(){
-//     var buttons = $('#buttons button');
-
-//     buttons.on('click', function(e){
-//       var $this = $(this);
-//       buttons.removeClass('selected').addClass('disabled');
-//       $this.addClass('selected').removeClass('disabled');
-
-//       switch ($this.attr('id')){
-//           case 'yes':
-//               val = true;
-//               break;
-//           case 'no':
-//               val = false;
-//               break;
-//       }
-//       //do something with the list
-//     })
-//   }
+for (let i = 0; i < checkBtns.length; i++) {
+  function checkBtn() {
+    checkBtns[i].classList.add("checked-btn");
+    checkBtns[i].classList.remove("unchecked-btn");
+    console.log("checkBtns[i].textContent");
+  }
+  checkBtns[i].addEventListener("click", checkBtn);
+  console.log(checkBtns[i]);
+}
