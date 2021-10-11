745d9ff7e1bb9ef6076b2f7c9f80d3aab761d0b9
kipchirchiralb <albertkipchirchir02@gmail.com>
2021-10-11 17:03:57 +0300

    all set

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const addTaskBtn = document.querySelector("#add-task-btn");
 const clearInputField = document.querySelector("#clear-input-field");
 const showCompleteBtn = document.querySelector("#show-complete-btn");
 const hideCompleteBtn = document.querySelector("#hide-complete-btn");
-const checkBtns = document.querySelectorAll(".unchecked-btn");
+const checkBtns = document.querySelectorAll(".check-btn");
 const completeTasks = document.querySelectorAll(".checked-btn");
 const clearList = document.querySelector("#clear-all-tasks");
 
@@ -32,6 +32,11 @@ clearList.addEventListener("click", function (e) {
 showCompleteBtn.addEventListener("click", showCompleteTasks);
 hideCompleteBtn.addEventListener("click", hideCompleteTasks);
 
+// check buttons
+for (let i = 0; i < checkBtns.length; i++) {
+  checkBtns[i].addEventListener("click", checkBtn);
+}
+
 // FUNCTIONS
 
 function addTaskToList() {
@@ -40,14 +45,12 @@ function addTaskToList() {
   } else {
     let text = addTaskInput.value;
     // if (text!=items[i]) - store items in local storage
-    let newItem = document.createElement("li");
-    let indicatorBtn = document.createElement("button");
-    indicatorBtn.textContent = "";
-    indicatorBtn.classList.add("unchecked-btn");
-    let textNode = document.createTextNode(text);
-    newItem.append(textNode);
-    newItem.appendChild(indicatorBtn);
-    myList.appendChild(newItem);
+    let newTask = `<li>
+    <input type="checkbox" id="checkbox" name="checkbox">
+    <label for = "checkbox">${text}</label>
+    </li>`;
+
+    myList.append(newTask.value);
     addTaskInput.value = "";
   }
 }
@@ -55,21 +58,20 @@ function addTaskToList() {
 function showCompleteTasks() {
   hideCompleteBtn.classList.remove("hide");
   showCompleteBtn.classList.add("hide");
-  completeTasks.classList.remove("hide");
 }
 
 function hideCompleteTasks() {
   showCompleteBtn.classList.remove("hide");
   hideCompleteBtn.classList.add("hide");
-  completeTasks.classList.add("hide");
 }
 
-for (let i = 0; i < checkBtns.length; i++) {
-  function checkBtn() {
-    checkBtns[i].classList.add("checked-btn");
-    checkBtns[i].classList.remove("unchecked-btn");
-    console.log("checkBtns[i].textContent");
+function checkBtn() {
+  for (let i = 0; i < checkBtns.length; i++) {
+    if (checkBtns[i].classList.hasOwnProperty("unchecked-btn")) {
+      console.log("unchecked button");
+    }
   }
-  checkBtns[i].addEventListener("click", checkBtn);
-  console.log(checkBtns[i]);
+  // checkBtns[i].classList.add("checked-btn");
+  // checkBtns[i].classList.remove("unchecked-btn");
+  // console.log("checkBtns[i].textContent");
 }
